793f5f88532fc0d2fe7ef75694010e2fec95b99d
nurnoby admin <nurnobyrahaman12588@outlook.com>
2024-12-10 22:31:33 +0600

    using Router for user and course

--- src/app.mjs
+++ src/app.mjs
@@ -8,12 +8,39 @@ app.use(express.json());
 
 app.use(cors());
 
+// router
+const userRouter = express.Router();
+const courseRouter = express.Router();
+
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/courses", courseRouter);
+
+userRouter.get("/create_user", (req, res) => {
+  const user = req.body;
+  console.log(user);
+  res.json({
+    success: true,
+    data: user,
+  })
+})
+
+courseRouter.post("/create_course", (req, res) => {
+  const course = req.body;
+  console.log(course);
+  res.json({
+    success: true,
+    data: course
+  })
+})
+
 //middleware
 
 const logger = (req, res, next) => {
   console.log(req.url, req.method, req.hostname)
   next()
 }
+
+
 app.get("/",logger, (req, res) => {
   console.log(req.query)
   res.send("Hello World!");
